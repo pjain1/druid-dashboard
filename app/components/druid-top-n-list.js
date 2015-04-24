01d7e474133c60a3af185e7a243b3e9d10cb8ec0
Mike North <northm@yahoo-inc.com>
2015-04-24 11:03:18 -0700

    use ember-new-computed for 1.11 compability

--- app/components/druid-top-n-list.js
+++ app/components/druid-top-n-list.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
+import computed from 'ember-new-computed';
 
 export default Ember.Component.extend({
   classNames: ['druid-top-n-list'],
-  title: Ember.computed('dimension', 'rankingMetric', {
+  title: computed('dimension', 'rankingMetric', {
     get() {
       return `Top ${this.get('dimension')} by ${this.get('rankingMetric')}`;
     }
